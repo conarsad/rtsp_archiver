50f0815f2cf34d3b710cd5ae86410fc82d2daee9
conarsad <40657636+conarsad@users.noreply.github.com>
2018-11-09 14:34:47 +0600

    Update rtsp_dump.sh

--- rtsp_dump.sh
+++ rtsp_dump.sh
@@ -11,5 +11,5 @@ fi
 if ! [ -d $storage_dir/$destdate ]; then
 mkdir $storage_dir/$destdate
 fi
-#ffmpeg -t 00:01:00 -i rtsp://login:password@192.168.1.3/ -vcodec copy -an "$storage_dir/$destdate/$desttime.mp4" 2> /dev/null
+ffmpeg -t 00:01:00 -i rtsp://login:password@192.168.1.3/ -vcodec copy -an "$storage_dir/$destdate/$desttime.mp4" 2> /dev/null
 done
